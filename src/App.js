82c1bfef332eb83b4e0c503fcb9d9b33e0a76529
shoaibalirts <shoaibali.rts@gmail.com>
2024-09-25 09:50:58 +0200

    created project using npx create-react-app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Greeting from "./components/Greeting.js";
+function App() {
+  return (
+    <div className="App">
+      <Greeting />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Greeting.js
@@ -0,0 +1,9 @@
+const Greeting = () => {
+  return (
+    <div>
+      <h2>Hello to React Jest Automattion</h2>
+      <p>Please learn it in-depth</p>
+    </div>
+  );
+};
+export default Greeting;
